700ba688ea6a5ad9c3f799beab9afc15be7d9b73
Bruno Caldas <brunopaivacaldas@hotmail.com>
2017-10-21 02:03:35 -0200

    Criação das classes de manipulação de DB e sessões

---
+++ lib/Session.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace lib;
+
+class Session
+{
+
+    private static $instancia = array();
+
+    /**
+     *
+     * @return Session
+     */
+    public static function getInstance()
+    {
+
+        if (self::$instancia = !null) {
+            self::$instancia = new Session();
+        }
+
+        return self::$instancia;
+    }
+
+    public function set($chave, $valor)
+    {
+        $_SESSION[$chave] = $valor;
+    }
+
+    public function get($chave)
+    {
+        $value = $_SESSION[$chave];
+        return $value;
+    }
+
+    public function verify($chave)
+    {
+        if (isset($_SESSION[$chave]) && (!empty($_SESSION[$chave]))) {
+
+            return true;
+        } else {
+
+            return false;
+        }
+    }
+
+    public function destroy()
+    {
+        unset($_SESSION);
+        session_destroy();
+    }
+}
