052f9d55a1f58929009b2b29ec06a89eac63e5d9
Bruno Caldas <brunopaivacaldas@hotmail.com>
2017-10-21 02:51:23 -0200

    Criação do model de usuário

---
+++ dao/UsuarioDao.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bruno
+ * Date: 21/10/2017
+ * Time: 02:49
+ */
+
+class UsuarioDao
+{
+
+}
---
+++ model/Usuario.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bruno
+ * Date: 21/10/2017
+ * Time: 02:36
+ */
+
+namespace model;
+
+
+class Usuario
+{
+    private $id;
+    private $nome;
+    private $login;
+    private $senha;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @param mixed $nome
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    /**
+     * @param mixed $login
+     */
+    public function setLogin($login)
+    {
+        $this->login = $login;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSenha()
+    {
+        return $this->senha;
+    }
+
+    /**
+     * @param mixed $senha
+     */
+    public function setSenha($senha)
+    {
+        $this->senha = $senha;
+    }
+}
